c3505de41bf7f659ce812250c5ffc58b14847867
slavita <slavita2014@yandex.ru>
2018-12-28 15:52:00 +0200

    Views
    разработка файлов вида по работе c страницами

---
+++ meta/_search.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\MetaSearch */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="meta-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+    ]); ?>
+
+    <?= $form->field($model, 'id') ?>
+
+    <?= $form->field($model, 'title') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton('Reset', ['class' => 'btn btn-default']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
