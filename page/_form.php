b95a37ece5dc4ec293f0b0f83594956fb2b5cedc
slavita <slavita2014@yandex.ru>
2018-12-28 15:52:47 +0200

    Views
    разработка файлов вида по работе c страницами

---
+++ page/_form.php
@@ -0,0 +1,38 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+use dosamigos\ckeditor\CKEditor;
+
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Page */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="page-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
+
+    <?php // $form->field($model, 'content')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'content')->widget(CKEditor::className(), [
+        'options' => ['rows' => 6],
+        'preset' => 'basic'
+    ]) ?>
+
+    <?= $form->field($model, 'created')->textInput() ?>
+
+    <?= $form->field($model, 'status')->textInput() ?>
+
+    <?= $form->field($model, 'category_id')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
