5a25bf14df087b924c0a4c1b4c79e6e97add148e
slavita <slavita2014@yandex.ru>
2018-12-28 15:57:20 +0200

    Views

---
+++ user/_search.php
@@ -0,0 +1,41 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\UserSearch */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="user-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+    ]); ?>
+
+    <?= $form->field($model, 'id') ?>
+
+    <?= $form->field($model, 'username') ?>
+
+    <?= $form->field($model, 'password') ?>
+
+    <?= $form->field($model, 'created') ?>
+
+    <?= $form->field($model, 'ban') ?>
+
+    <?php // echo $form->field($model, 'role') ?>
+
+    <?php // echo $form->field($model, 'email') ?>
+
+    <?php // echo $form->field($model, 'auth_key') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton('Reset', ['class' => 'btn btn-default']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
